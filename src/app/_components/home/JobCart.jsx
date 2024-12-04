5637955f8d9434ba98ee5b75c5513bd4bdc5d01c
naiemhosen430 <naiemhosen430@gmail.com>
2024-12-04 19:44:28 +0600

    d

--- src/app/_components/home/JobCart.jsx
+++ src/app/_components/home/JobCart.jsx
@@ -12,25 +12,25 @@ export default function JobCart({data}) {
 
     <div onClick={() => set_job(data)} className="w-full cursor-pointer mx-auto p-4 bg-none border border-gray-500 my-2 rounded-lg shadow-md">
       {/* Cart Header (Title) */}
-        <h2 className="text-2xl font-bold text-right text-white">{data?._doc?.profession}</h2>
+        <h2 className="text-2xl font-bold text-right text-white">{data?.profession}</h2>
       <div className="mb-4 flex justify-between items-center">
-        <h2 className="text-xl font-semibold text-white">{data?._doc?.description}</h2>
+        <h2 className="text-xl font-semibold text-white">{data?.description}</h2>
         
       </div>
 
       {/* Cart Body (Location and Type) */}
       <div className="flex justify-between items-center text-gray-500 mb-4">
         <div className="flex text-gray-100 text-[10px] w-6/12 items-center">
-          <span>{data?._doc?.area}</span>
+          <span>{data?.area}</span>
         </div>
         <div className="flex text-gray-100 text-[10px] w-2/12 items-center">
-          <span>{data?._doc?.status}</span>
+          <span>{data?.status}</span>
         </div>
         <div className="flex text-gray-100 text-[10px] w-2/12 items-center">
-          <span>{data?._doc?.need_type}</span>
+          <span>{data?.need_type}</span>
         </div>
         <div className="flex text-gray-100 text-[10px] w-2/12 items-center">
-          <span>{data?._doc?.type}</span>
+          <span>{data?.type}</span>
         </div>
       </div>
 
