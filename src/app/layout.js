f64d547e6489e2882cdfa62b5313b2864f723aa2
naiemhosen430 <naiemhosen430@gmail.com>
2025-03-05 10:01:54 -0800

    k

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
           <NotificationContextProvider>
             <MessageContextProvider>
               <ThemeProvider theme={theme}>
-                <div className="min-h-screen bg-gray-100">
+                <div className="min-h-screen bg-[#202a3a]">
 
                 <Header />
                 <div className="px-0">
